a248796664330f34840d69c0d27d90a10abd07f2
Boyles, Prince Niño L. <94779042+JesseMc87@users.noreply.github.com>
2025-07-29 08:02:46 +0800

    Add files via upload

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,9 +1,15 @@
+import { Image } from 'expo-image';
 import { StyleSheet, Text, View } from 'react-native';
-
+const PlaceholderImage = require('@/assets/images/pic2.jpg');
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>About screen</Text>
+      <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+    </View>
+      
       <Text style={styles.text}>Dev: Boyles, Prince Niño</Text>
     </View>
   );
@@ -18,5 +24,16 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#fff',
+    paddingBottom: 200,
+
+  },
+   imageContainer: {
+    flex: 1,
+  },
+  image: {
+    paddingTop: 10,
+    width: 320,
+    height: 300,
+    borderRadius: 18,
   },
 });
